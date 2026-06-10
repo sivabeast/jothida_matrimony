2811506f89ab93aad65dd2815fcfb14b69971165
sivabeast <sivabeast123123@gmail.com>
2026-06-10 19:01:52 +0530

    Fix l10n codegen: output to lib/l10n (Flutter 3.41 removed flutter_gen synthetic package)

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:jothida_matrimony/l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/theme/app_theme.dart';
 import 'firebase_options.dart';
